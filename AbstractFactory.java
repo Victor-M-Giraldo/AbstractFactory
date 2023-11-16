5fef80aae7bc63b9df419b8c44c4bc8e75a1619e
Victor Giraldo <117304729+Victor-M-Giraldo@users.noreply.github.com>
2023-11-16 17:21:35 -0500

    Update AbstractFactory.java
    Used proper method for creating a NY cheese pizza and a chicago veggie pizza

--- AbstractFactory.java
+++ AbstractFactory.java
@@ -3,20 +3,22 @@ public class AbstractFactory{
 
     public static void main(String[] args) {
 
+
+
         PizzaStore NyStore = new NYPizzaStore();
-        Pizza NyTest = NyStore.createPizza("cheese");
-        System.out.println(NyTest.toString());
+    
 
         PizzaStore ChicagoStore = new ChicagoPizzaStore();
-        Pizza ChicagoTest = ChicagoStore.createPizza("veggie");
-        System.out.println(ChicagoTest.toString());
 
-        PizzaStore NyStoreTwo = new NYPizzaStore();
-        Pizza NyTestTwo = NyStoreTwo.createPizza("pepperoni");
-        System.out.println(NyTestTwo.toString());
+        Pizza nyPizza = NyStore.orderPizza("cheese");
 
-        PizzaStore ChicagoStoreTwo = new ChicagoPizzaStore();
-        Pizza ChicagoTestTwo = ChicagoStoreTwo.createPizza("clam");
-        System.out.println(ChicagoTestTwo.toString());
+        System.out.println("Ordered a pizza from NY: " + nyPizza);
+
+        Pizza ChicagoPizza = ChicagoStore.orderPizza("veggie");
+
+        System.out.println("Ordered a pizza from Chicago " + ChicagoPizza);
+
+
+    
     }
 }
